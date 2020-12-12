95754fc371a446d5ee95f2fd7cd2fc2d2c6f7b72
QUDUSKUNLE <quduskunle@gmail.com>
2020-12-12 10:03:25 +0100

    Intro to typeScript

---
+++ src/basicTypes.ts
@@ -0,0 +1,38 @@
+let isDone: boolean = false;
+
+let decimal: number = 5;
+
+let color: string = 'My name';
+
+// Array of numbers
+let list: number[] = [1,2,3,4];
+
+// Other way of representing
+let list1: Array<number> = [1,2,3];
+
+// Tuple
+let x: [string, number] = ['Hello', 5];
+
+// Enum
+enum Color {
+  Red,
+  Green,
+  Blue,
+}
+
+// Unknown
+// This assume it can be anything
+let now: unknown = 4;
+
+// Any
+// Maybe opt out of type checking
+// We use Any
+declare function getValue(key: string): any {
+  return key;
+}
+const str: string = getValue('my Name');
+
+// Void
+function warnUser(): void {
+  console.log('This is a warning message') 
+}
